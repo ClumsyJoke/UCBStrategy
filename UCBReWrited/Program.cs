9c5b1f1ca4c20a9d6b4b64547be9d68edef8f5c5
Альберт Ворошилов <s244528@std.novsu.ru>
2022-05-07 12:43:37 +0300

    Добавьте файлы проекта.

---
+++ UCBReWrited/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace UCBReWrited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrategyАnalysis[] arrStrat = new StrategyАnalysis[3];
+            Thread[] potok1 = new Thread[2];
+
+            arrStrat[0] = new StrategyАnalysis(new StreamWriter("D:/Out11000.txt"));
+            arrStrat[1] = new StrategyАnalysis(new StreamWriter("D:/Out21000.txt"));
+            arrStrat[2] = new StrategyАnalysis(new StreamWriter("D:/Out31000.txt"));
+
+            potok1[0] = new Thread(() => arrStrat[0].FindOptimalA(0.6d));
+            potok1[1] = new Thread(() => arrStrat[1].FindOptimalA(0.8d));
+            potok1[0].Start();
+            potok1[1].Start();
+            arrStrat[2].FindOptimalA(1d);
+        }
+    }
+}
---
+++ UCBReWrited/StrategyАnalysis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+
+namespace UCBReWrited
+{
+    class StrategyАnalysis
+    {
+        UCBStrategy strat;
+        const int averagingResult = 10000;
+        //double startA;
+
+
+        public StrategyАnalysis(StreamWriter ss) 
+        {
+            strat = new UCBStrategy(2,1000,300000,ss);
+
+        }
+
+        public void FindOptimalA(double startA)
+        {
+            double minMaxLost = 10;
+            double lost;
+            double optimalA = 1;
+            for (double a = startA;a < startA + 0.2d; a += 0.03) 
+            {
+                Console.WriteLine("a = {0}", a);
+                strat.sw.WriteLine("a = {0}", a);
+                lost = strat.PlayStrategy(a);
+                Console.WriteLine("Max lost = {0}", lost);
+                if (minMaxLost > lost)
+                {
+                    minMaxLost = lost;
+                    optimalA = a;
+                }
+            }
+            Console.WriteLine("Optimal a = {0} with max lost {1}", optimalA, minMaxLost);
+            strat.sw.Close();
+        }
+    }
+}
